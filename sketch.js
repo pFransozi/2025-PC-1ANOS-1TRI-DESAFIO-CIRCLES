0c217a61c49098a587a68cce5a0e25c6ab44c246
Philipe Fransozi <philipehf@gmail.com>
2025-02-09 09:17:46 -0300

    add desafio-circulo

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  //círculos na primeira linha
+  //circle(x,y,d)
+  circle(50,50,100)
+  circle(150,50,100)
+  circle(250,50,100)
+  circle(350,50,100)
+  //círculos na segunda linha
+  circle(50,350,100)
+  circle(150,350,100)
+  circle(250,350,100)
+  circle(350,350,100)
+}
